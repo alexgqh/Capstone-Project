24e55bace24e171a284ba437ac25e376f196d704
alexgqh <gymalamo@yahoo.com>
2026-01-13 15:42:19 -0400

    Set up basic structure for 'Testimonials' section.

--- src/components/testimonials.jsx
+++ src/components/testimonials.jsx
@@ -5,7 +5,13 @@ const Testimonials = () => {
     <section id="testimonials">
       <hr className="color-charcoal" />
       <div className="space-block-64" aria-hidden="true" />
-      <h1 className="color-charcoal" style={{textAlign: "center"}}>Testimonials</h1>
+      <div id="testimonials-display">
+        <h1 className="color-charcoal" style={{textAlign: "center", gridColumn: "1 / -1"}}>Testimonials</h1>
+        <div className="testimonial">testimonial 1</div>
+        <div className="testimonial">testimonial 2</div>
+        <div className="testimonial">testimonial 3</div>
+        <div className="testimonial">testimonial 4</div>
+      </div>
       <div className="space-block-64" aria-hidden="true" />
       <hr className="color-charcoal" />
     </section>
