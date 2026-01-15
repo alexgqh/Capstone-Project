4cbed98745742a406caa2c44fce1aa4d1add9101
alexgqh <gymalamo@yahoo.com>
2026-01-15 18:54:07 -0400

    Fixed tab formatting in special.jsx

--- src/components/special.jsx
+++ src/components/special.jsx
@@ -10,17 +10,17 @@ const Special = ({id, image, alt, title, desc, price}) => {
    }
 
    return (
-      <div className="special" style={{overflow:"hidden"}}>
-         <img src={image} alt={alt} style={{minHeight: "192px", maxHeight: "192px", objectFit: "cover"}} />
-         <div className="special-info" style={{...styleSpecialTitle, flexDirection:"column", height:"100%"}}>
-            <div className="special-title" style={ {...styleSpecialTitle, ...gapBottom} }>
-               <h4 className="color-charcoal">{title}</h4>
-               <h4 className="color-peach">{price}</h4>
-            </div>
-            <p className="color-charcoal" style={gapBottom}>{desc}</p>
-            <a style={{...styleSpecialTitle}}>Order for delivery<img src={IconGo} /></a>
-         </div>
+    <div className="special" style={{overflow:"hidden"}}>
+      <img src={image} alt={alt} style={{minHeight: "192px", maxHeight: "192px", objectFit: "cover"}} />
+      <div className="special-info" style={{...styleSpecialTitle, flexDirection:"column", height:"100%"}}>
+        <div className="special-title" style={ {...styleSpecialTitle, ...gapBottom} }>
+          <h4 className="color-charcoal">{title}</h4>
+          <h4 className="color-peach">{price}</h4>
+        </div>
+        <p className="color-charcoal" style={gapBottom}>{desc}</p>
+        <a style={{...styleSpecialTitle}}>Order for delivery<img src={IconGo} /></a>
       </div>
+    </div>
    )
 }
 
