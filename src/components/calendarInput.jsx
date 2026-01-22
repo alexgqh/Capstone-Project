b910f01fecf862ed346ba521a2aeffaa3b7cbb92
alexgqh <gymalamo@yahoo.com>
2026-01-22 16:13:25 -0400

    Began working on the calendar dropdown

--- src/components/calendarInput.jsx
+++ src/components/calendarInput.jsx
@@ -1,4 +1,6 @@
 import IconCalendar from '../assets/icon-calendar.svg';
+import IconArrowLeft from '../assets/icon-arrow-left.svg';
+import IconArrowRight from '../assets/icon-arrow-right.svg';
 import InputBase from './inputBase';
 import { useState, useRef } from 'react';
 
@@ -26,10 +28,45 @@ const CalendarInput = ({ id, caption, placeholder, bookingThresholdDays = 30 }) 
     }
   }
 
+  const handleMonthLeft = (e) => { e.preventDefault(); }
+  const handleMonthRight = (e) => { e.preventDefault(); }
+
+  const dayLabels = ["S","M","T","W","Th","F","Sa"]
+
   function renderDropdown() {
+    function renderDays() {
+      let rows = [];
+      //get month
+      const currentDate = new Date();
+      const currentMonth = currentDate.toLocaleString('default', { month: "long" });
+
+      //get day of 1st of month
+
+      //save days of row 1
+      rows.push([30,31,1,2,3,4,5]);
+      //save days of row 2
+      rows.push([6,7,8,9,10,11,12]);
+      //save days of row 3
+      rows.push([13,14,15,16,17,18,19]);
+      //save days of row 4
+      rows.push([20,21,22,23,24,25,26]);
+      //save days of row 5 (unless we're in a 28-day February where the 1st is on a Sunday)
+      rows.push([27,28,29,30,31,1,2]);
+
+      //render rows 1-5
+      return rows.map(row => row.map(day => <span className="calendar-day">{day}</span>));
+    }
     return (
       <div className="input-calendar-dropdown" ref={dropdownRef}>
-        &lt; January &gt;
+        <div className="calendar-dropdown-title">
+          <button onClick={handleMonthLeft}><img src={IconArrowLeft} alt="Left arrow"/></button>
+          <h3 className="color-green">January</h3>
+          <button onClick={handleMonthRight}><img src={IconArrowRight} alt="Right arrow"/></button>
+        </div>
+        <div className="calendar-dropdown-grid">
+          {dayLabels.map(label => <span className="calendar-day-label">{label}</span>)}
+          {renderDays()}
+        </div>
       </div>
     );
   }
