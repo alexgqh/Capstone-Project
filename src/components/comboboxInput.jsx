2061c1d2c1fb9c2ab72d08ab54ec2d9cf3b01b11
alexgqh <gymalamo@yahoo.com>
2026-01-28 09:15:20 -0400

    Made ComboboxInput's dropdown options selectable.

--- src/components/comboboxInput.jsx
+++ src/components/comboboxInput.jsx
@@ -1,23 +1,55 @@
+import { useState, useRef } from "react"
 import InputBase from "./inputBase"
 import Placeholder from "./placeholder"
 
 const ComboboxInput = ({ id, caption, isRequired, placeholder, options }) => {
-  function renderOptions() {
-    return options.map((option, i) => {
-      let className = "input-dropdown-item combobox-dropdown-item input-font"
-      if (i < options.length - 1) {
-        className += " line-beneath"
-      }
-      return <div className={className}>{option}</div>
-    })
+  const [expanded, setExpanded] = useState(false)
+  const [selected, setSelected] = useState(null)
+  const dropdownRef = useRef()
+
+  function renderDropdown() {
+    function renderDropdownItems() {
+      return options.map((option, i) => {
+        let className = "input-dropdown-item combobox-dropdown-item input-font"
+        if (i < options.length - 1) {
+          className += " line-beneath"
+        }
+        const handleClick = () => {
+          setSelected(i)
+          setExpanded(false)
+        }
+        return <div className={className} onClick={handleClick} role="option">{option}</div>
+      })
+    }
+    return (expanded &&
+      <div className="input-dropdown combobox-dropdown" ref={dropdownRef}>
+        {renderDropdownItems()}
+      </div>
+    )
   }
+
+  const handleClick = (e) => {
+    if (dropdownRef.current?.contains(e.target)) return
+    setExpanded(!expanded);
+  }
+
+  let style = {}
+  if (expanded) style.zIndex = "1000"
+
+  function renderPlaceholderOrSelected() {
+    if (selected === null) {
+      return <Placeholder>{placeholder}</Placeholder>
+    } else {
+      return <p className="input-font">{options[selected]}</p>
+    }
+  }
+
   return (
     <>
-      <InputBase id={id} className="input-dropdown-parent" caption={caption} isRequired={isRequired} role="combobox">
-        <Placeholder>{placeholder}</Placeholder>
-        <div className="input-dropdown combobox-dropdown">
-          {renderOptions()}
-        </div>
+      {expanded && <div className="fullscreen-mask" onClick={() => setExpanded(false)} />}
+      <InputBase id={id} className="input-dropdown-parent" caption={caption} isRequired={isRequired} onClick={handleClick} role="combobox" style={style}>
+        {renderPlaceholderOrSelected()}
+        {renderDropdown()}
       </InputBase>
     </>
   )
